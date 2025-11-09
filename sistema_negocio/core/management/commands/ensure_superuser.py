733151ae0a44e613ed1f8ba2381c848dedb63fb9
esr-sosa <sosaemanuel4436@gmail.com>
2025-11-09 15:43:01 -0300

    Handle invalid emails when resetting superuser

--- sistema_negocio/core/management/commands/ensure_superuser.py
+++ sistema_negocio/core/management/commands/ensure_superuser.py
@@ -5,7 +5,9 @@ from typing import Any
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
+from django.core.validators import validate_email
 
 
 class Command(BaseCommand):
@@ -46,6 +48,22 @@ class Command(BaseCommand):
                     ", ".join(missing_fields)
                 )
             )
+
+        # Valida el correo electrónico y, si es posible, permite corregirlo en modo interactivo.
+        while True:
+            try:
+                validate_email(email)
+                break
+            except ValidationError:
+                if noinput:
+                    raise CommandError(
+                        "El correo electrónico proporcionado ('{}') no es válido. Proporciona uno válido "
+                        "mediante --email o DJANGO_SUPERUSER_EMAIL.".format(email)
+                    )
+                email = input("Correo electrónico válido para '{}': ").strip()
+                if not email:
+                    self.stdout.write(self.style.WARNING("El correo no puede quedar vacío."))
+                    continue
 
         if not password:
             if noinput:
